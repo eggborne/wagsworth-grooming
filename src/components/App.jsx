ed4787b1d2c7c73d275c9102e83a0dc970866006
Michael Donovan <mike@eggborne.com>
2018-11-11 05:18:21 -0800

    Configure to retrieve lists from database

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,41 +2,55 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import ListIndex from './ListIndex';
+import NewEntryFormIndex from './NewEntryFormIndex';
 var uuid = require('uuid/v1');
 var moment = require('moment');
 import { Switch, Route } from 'react-router-dom';
 
-let phonyKeys = [
-  uuid(),
-  uuid({
-    msecs: 100,
-    nsecs: 3000
-  }),
-  uuid({
-    msecs: 300,
-    nsecs: 2000
-  }),
-  uuid({
-    msecs: 340,
-    nsecs: 6000
-  }),
-  uuid({
-    msecs: 400,
-    nsecs: 7500
-  }),
-  uuid({
-    msecs: 500,
-    nsecs: 3546
-  }),
-  uuid({
-    msecs: 120,
-    nsecs: 150
-  }),
-  uuid({
-    msecs: 360,
-    nsecs: 758
-  }),
-];
+const entryAttributes = {
+  pets: [
+    'name',
+    'breed',
+    'sex',
+    'color',
+    'weight',
+    'dob',
+    'vaccinationDate',
+    'vaccinationClinic',
+    'veterinarian',
+    'parent',
+    'notes',
+  ],
+  parents: [
+    'lastName',
+    'firstNames',
+    'phoneNumbers',
+    'email',
+    'notes',
+    'petIds',
+    'upcomingApptIds',
+    'pastApptIds',
+  ],
+  appointments: [
+    'employeeId',
+    'petId',
+    'date',
+    'startTime',
+    'services',
+    'notes',
+  ],
+  employees: [
+    'role',
+    'firstName',
+    'lastName',
+    'phoneNumbers',
+    'email',
+    'upcomingApptIds',
+    'pastApptIds',
+    'schedule',
+    'notes'
+  ]
+};
 
 class App extends React.Component {
 
@@ -45,143 +59,130 @@ class App extends React.Component {
     this.state = {
       newFormRequested: null,
       lists: {
-        employees: {
-          [phonyKeys[5]]: {
-            id: phonyKeys[5],
-            firstName: 'Regina',
-            lastName: 'Falangy',
-            phoneNumber: '360-867-5309',
-            upcomingApptIds: [4],
-            pastApptIds: [1, 4],
-            notes: 'Kind of snooty',
-            schedule: 'TBD',
-            formattedTimeSince: '',
-            dateCreated: '2014-02-01'
-          },
-          [phonyKeys[6]]: {
-            id: phonyKeys[6],
-            firstName: 'Larry',
-            lastName: 'Johnson',
-            phoneNumber: '360-936-5589',
-            upcomingApptIds: [5, 6],
-            pastApptIds: [2, 3],
-            notes: 'Fabulous tipper',
-            schedule: 'TBD',
-            formattedTimeSince: '',
-            dateCreated: '1963-07-04'
-          }
-        },
-        parents: {
-          [phonyKeys[0]]: {
-            id: phonyKeys[0],
-            firstName: 'Deborah',
-            lastName: 'Watkins',
-            phoneNumber: '360-867-5309',
-            petIds: [1, 2],
-            upcomingApptIds: [4],
-            pastApptIds: [1, 4],
-            notes: 'Kind of snooty',
-            formattedTimeSince: '(not user-created)',
-            dateCreated: '2018-10-31'
-          },
-          [phonyKeys[1]]: {
-            id: phonyKeys[1],
-            firstName: 'Marcus',
-            lastName: 'Wellington',
-            phoneNumber: '360-936-5589',
-            petIds: [3],
-            upcomingApptIds: [5, 6],
-            pastApptIds: [2, 3],
-            notes: 'Fabulous tipper',
-            formattedTimeSince: '(not user-created)',
-            dateCreated: '2018-06-18'
-          }
-        },
-        pets: {
-          [phonyKeys[2]]: {
-            id: phonyKeys[2],
-            name: 'Megatron',
-            sex: 'M',
-            breed: 'Chihuahua / Pomeranian',
-            weight: 9,
-            color: 'Black / Brown',
-            dob: moment().subtract(8, 'months').format(),
-            vaccinationDate: 6042018,
-            vaccinationClinic: 0,
-            veterinarian: 0,
-            parent: phonyKeys[0],
-            notes: 'A good boy :)',
-            formattedTimeSince: '(not user-created)',
-            dateCreated: '2016-06-18'
-          },
-          [phonyKeys[3]]: {
-            id: phonyKeys[3],
-            name: 'Cheddar Cheese',
-            sex: 'F',
-            breed: 'Beagle',
-            weight: 9,
-            color: 'White / Gold',
-            dob: moment().subtract(5, 'years').subtract(9, 'months').format(),
-            vaccinationDate: 8092018,
-            vaccinationClinic: 1,
-            veterinarian: 1,
-            parent: phonyKeys[0],
-            notes: 'Slobbers',
-            formattedTimeSince: '(not user-created)',
-            dateCreated: moment().format()
-          },
-          [phonyKeys[4]]: {
-            id: phonyKeys[4],
-            name: 'Bob',
-            sex: 'M',
-            breed: 'Pitbull / Terrier',
-            weight: 65,
-            color: 'White',
-            dob: moment().subtract(12, 'years').subtract(3, 'months').format(),
-            vaccinationDate: 10312008,
-            vaccinationClinic: 2,
-            veterinarian: 1,
-            parent: phonyKeys[1],
-            notes: 'Sensitive pads',
-            formattedTimeSince: '(not user-created)',
-            dateCreated: moment().format()
-          }
-        },
-        appointments: {
-          'hgjhgf': {
-            id: 'hgjhgf',
-            employee: phonyKeys[6],
-            date: 10312018,
-            petId: 1,
-            startTime: 1530,
-            services: ['Kennel cut #10', 'Nail filing'],
-            notes: ['Shat on the table twice'],
-            formattedTimeSince: '(not user-created)',
-            dateCreated: moment().format()
-          },
-          'yeuyeur': {
-            id: 'yeuyeur',
-            employee: phonyKeys[5],
-            date: 11092017,
-            petId: 3,
-            startTime: 1000,
-            services: ['Flea dip', 'Deluxe ball-washing'],
-            notes: ['Fewer fleas than last time :D'],
-            formattedTimeSince: '(not user-created)',
-            dateCreated: moment().format()
-          }
-        }
+        employees: {},
+        parents: {},
+        pets: {},
+        appointments: {}
       }
     };
 
     this.printEntryLink = this.printEntryLink.bind(this);
-    this.cancelFormRequest = this.cancelFormRequest.bind(this);
+    this.addToLocalList = this.addToLocalList.bind(this);
+    this.saveNewEntryToDatabase = this.saveNewEntryToDatabase.bind(this);
+    this.getList = this.getList.bind(this);
+    this.getEntryById = this.getEntryById.bind(this);
     this.handleEntryFormRequest = this.handleEntryFormRequest.bind(this);
-    this.cancelFormRequest = this.cancelFormRequest.bind(this);
     this.handleSubmittingNewEntry = this.handleSubmittingNewEntry.bind(this);
   }
 
-  componentDidMount() { }
+  addToLocalList(listType, newEntryArray) {
+    let origLists = this.state.lists;
+    let newLists = Object.assign({}, origLists);
+
+    newEntryArray.forEach((entryObject) => {
+      for (let prop in entryObject) {
+        let val = entryObject[prop];
+        if (val[0] === '[') {
+          val = val.slice(1, val.length - 1);
+          val = val.split(',');
+        } else if (val.length === 0) {
+          val = [];
+        }
+        entryObject[prop] = val;
+      }
+      newLists[listType][entryObject.id] = entryObject;
+    });
+
+    this.setState({
+      lists: newLists,
+    });
+  }
+
+  saveNewEntryToDatabase(tableName, newEntryObj) {
+    
+    let dataObj = {
+      tableName: tableName,
+      uniqueId: uuid(),
+      dateCreated: moment().format()
+    };
+
+    entryAttributes[tableName].forEach((dataField) => {
+      dataObj[[dataField]] = newEntryObj[dataField];
+    });
+
+    $.ajax({
+      type: 'post',
+      url: `https://www.eggborne.com/scripts/saveentryto${tableName}.php`,
+      data: dataObj,
+      success: () => {
+        this.getList(tableName);
+      },
+      error: () => {
+        
+      }
+    });
+
+  }
+
+  getEntryById(listType, id) {
+    new Promise(
+      (resolve) => {
+        $.ajax({
+          type: 'get',
+          url: 'https://www.eggborne.com/scripts/getentrybyid.php',
+          data: {
+            listType: listType,
+            id: id,
+          },
+          success: (response) => {
+            let newEntryObj = JSON.parse(response);
+            resolve([listType,newEntryObj]);
+          },
+          error: function () {
+            
+          }
+        });
+      }
+    ).then((val) => {
+      this.addToLocalList(val[0], [val[1]]);
+    }).catch(() => {
+      
+    });
+  }
+
+  getList(listType, start, limit) {
+    new Promise(
+      (resolve) => {
+        $.ajax({
+          type: 'get',
+          url: 'https://www.eggborne.com/scripts/getlists.php',
+          data: {
+            listType: listType,
+            start: start,
+            limit: limit
+          },
+          success: (response) => {
+            let newEntryArray = JSON.parse(response);
+            resolve([listType,newEntryArray]);
+          }
+        });
+      }
+    ).then((val) => {
+      this.addToLocalList(val[0], val[1]);
+    }).catch(() => {
+      
+    });
+    // return promise;
+  }
+
+  componentDidMount() { 
+    // this.getList('employees');
+    // this.getList('parents');
+    // this.getList('pets');
+    // this.getList('appointments');
+
+    // this.getEntryById('parents','f134ae10-e3d9-11e8-8982-5fccabf17114')
+  }
   componentWillUnmount() { }
   UNSAFE_componentWillMount() { }
   UNSAFE_componentWillReceiveProps() { }
@@ -191,11 +192,24 @@ class App extends React.Component {
 
   printEntryLink(type, key) {
     let output;
-    if (key) {
+    if (key && this.state.lists[type][key]) {
       let entryObj = this.state.lists[type][key];
-      output = `${entryObj.firstName} ${entryObj.lastName}`;
+      if (type==='employees') {
+        output = `${entryObj.firstName} ${entryObj.lastName}`;
+      } else if (type === 'pets') {
+        output = `${entryObj.name}`;
+      } else if (type === 'parents') {
+        output = `${entryObj.lastName}`;
+      }
     } else {
-      output = 'Your Mama';
+      
+      if (key) {
+        console.log(`${key} NOT in local ${type} list!`);
+        this.getEntryById(type, key);
+        return;
+      } else {
+        output = 'No id passed';
+      }
     }
     return output;
   }
@@ -204,23 +218,11 @@ class App extends React.Component {
     this.setState({
       newFormRequested: type
     });
+    window.location.hash = '#/newentry';
   }
 
   handleSubmittingNewEntry(newEntryObj, type) {
-    let origLists = this.state.lists;
-    let newLists = Object.assign({}, origLists);
-    newEntryObj.id = newEntryObj.id = uuid();
-    newLists[type][newEntryObj.id] = newEntryObj;
-    this.setState({
-      lists: newLists,
-      newFormRequested: null
-    });
-  }
-
-  cancelFormRequest() {
-    this.setState({
-      newFormRequested: null
-    });
+    this.saveNewEntryToDatabase(type, newEntryObj);
   }
 
   render() {
@@ -238,7 +240,7 @@ class App extends React.Component {
       <div style={heightAdjusted} id='main'>
         <style jsx>{`
           #main {
-            background-color: var(--darkAccent);
+            background-color: white;
             font-family: Playfair Display; serif;
             position: relative
           }
@@ -251,29 +253,36 @@ class App extends React.Component {
           <Switch>
             <Route exact path='/' render={() => <ListIndex section={'splashPage'} />} />
             <Route path='/parents' render={() => <ListIndex section={'parents'}
-              displayList={this.state.lists['parents']}
+              handleUpdateListFromDB={this.getList}
+              lists={this.state.lists}
               printEntryLink={this.printEntryLink}
-              onHandleEntryFormRequest={this.handleEntryFormRequest}
+              onRequestNewEntryForm={this.handleEntryFormRequest}
               newFormRequested={this.state.newFormRequested}
-              onHandleSubmittingNewEntry={this.handleSubmittingNewEntry} />} />
+              onSubmitNewEntryForm={this.handleSubmittingNewEntry} />} />
             <Route path='/pets' render={() => <ListIndex section={'pets'}
-              displayList={this.state.lists['pets']}
+              handleUpdateListFromDB={this.getList}
+              lists={this.state.lists}
               printEntryLink={this.printEntryLink}
-              onHandleEntryFormRequest={this.handleEntryFormRequest}
+              onRequestNewEntryForm={this.handleEntryFormRequest}
               newFormRequested={this.state.newFormRequested}
-              onHandleSubmittingNewEntry={this.handleSubmittingNewEntry} />} />
+              onSubmitNewEntryForm={this.handleSubmittingNewEntry} />} />
             <Route path='/appointments' render={() => <ListIndex section={'appointments'}
-              displayList={this.state.lists['appointments']}
+              handleUpdateListFromDB={this.getList}
+              lists={this.state.lists}
               printEntryLink={this.printEntryLink}
-              onHandleEntryFormRequest={this.handleEntryFormRequest}
+              onRequestNewEntryForm={this.handleEntryFormRequest}
               newFormRequested={this.state.newFormRequested}
-              onHandleSubmittingNewEntry={this.handleSubmittingNewEntry} />} />
+              onSubmitNewEntryForm={this.handleSubmittingNewEntry} />} />
             <Route path='/employees' render={() => <ListIndex section={'employees'}
-              displayList={this.state.lists['employees']}
+              handleUpdateListFromDB={this.getList}
+              lists={this.state.lists}
               printEntryLink={this.printEntryLink}
-              onHandleEntryFormRequest={this.handleEntryFormRequest}
+              onRequestNewEntryForm={this.handleEntryFormRequest}
               newFormRequested={this.state.newFormRequested}
-              onHandleSubmittingNewEntry={this.handleSubmittingNewEntry} />} />
+              onSubmitNewEntryForm={this.handleSubmittingNewEntry} />} />
+            <Route path='/newentry' render={() => <NewEntryFormIndex type={this.state.newFormRequested}
+              onFormSubmission={this.handleSubmittingNewEntry}
+              lists={this.state.lists} />} />
           </Switch>
         </div>
         <Footer />
