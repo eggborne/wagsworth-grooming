280a5623918994d7a418baf1529c9496a5f7c8cb
Michael Donovan <mike@eggborne.com>
2018-11-13 14:09:13 -0800

    Adjust styles

--- src/components/SectionHeading.jsx
+++ src/components/SectionHeading.jsx
@@ -8,19 +8,27 @@ function SectionHeading(props) {
       <style jsx>{`
         div {
           border-radius: 0.5rem;
-          background-color: #222;
+          background-color: var(--dark);
+          text-shadow: 1px 1px 1px black;
           font-family: Tangerine, cursive;
           font-size: 2.25rem;
-          color: #eee;
+          color: var(--mainBg);
           width: 100%;
           padding: 2%;
           display: inline-flex;
           align-items: center;
         }
+        #btns {
+          display: inline-flex;
+          justify-content: flex-end;
+        }
         button {
+          min-width: 5rem;
+          padding: 0.5rem 0.2rem 0.5rem 0.2rem;
           font-size: 0.8rem;
           border-radius: 0.4rem;
           margin: 0.2rem;
+          
         }
       `}</style>
       <div id='lbl'>{headerText}</div>
