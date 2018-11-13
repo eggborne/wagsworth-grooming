6c5c27332078b25caf0dbe8a87c9a2aed40b36c9
Michael Donovan <mike@eggborne.com>
2018-11-12 21:21:52 -0800

    Expand hamburger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,12 +13,32 @@ class Header extends React.Component {
 
     this.changeSearchList = this.changeSearchList.bind(this);
     this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+    this.toggleMenuItemOn = this.toggleMenuItemOn.bind(this);
 
   }
 
   changeSearchList(newList) {
     this.setState({
       searchList: newList
+    });
+  }
+
+  toggleMenuItemOn(newItem) {
+    Array.from(document.getElementsByClassName('mainMenuItem')).map((menuItem, i) => {
+      if (menuItem.id === newItem) {
+        menuItem.style.backgroundColor = '#222';
+      } else {
+        menuItem.style.backgroundColor = 'transparent';
+      }
+    });
+    let newList = newItem.replace('MenuArea', 's')
+    this.setState({
+      searchList: newList
+    })
+    Array.from(document.getElementsByName('search-type')).map((searchType, i) => {
+      if (searchType.value === newList) {
+        searchType.checked = true;
+      }
     });
   }
 
@@ -36,20 +56,34 @@ class Header extends React.Component {
   }
 
   render() {
-
+    let menuItemStyle = {
+      backgroundColor: 'none',
+      padding: '0.5rem',
+      borderRadius: '0.5rem',
+      paddingRight: '0.75rem'
+    }
     let barContents;
     let displayListNav;
     if (this.props.menuSymbol === 'close') {
       displayListNav = 'block';
       barContents =
         <div>
-          <Link to="/employees">Employees</Link> | <Link to="/parents">Parents</Link> | <Link to="/pets">Pets</Link> | <Link to="/appointments">Appointments</Link>
+          <div className='mainMenuItem' id='employeeMenuArea' onClick={() => this.toggleMenuItemOn('employeeMenuArea')} style={menuItemStyle}><Link to="/employees">Employees</Link></div>
+          <div className='mainMenuItem' id='parentMenuArea' onClick={() => this.toggleMenuItemOn('parentMenuArea')} style={menuItemStyle}><Link to="/parents">Parents</Link></div>
+          <div className='mainMenuItem' id='petMenuArea' onClick={() => this.toggleMenuItemOn('petMenuArea')} style={menuItemStyle}><Link to="/pets">Pets</Link></div>
+          <div className='mainMenuItem' id='appointmentMenuArea' onClick={() => this.toggleMenuItemOn('appointmentMenuArea')} style={menuItemStyle}><Link to="/appointments">Appointments</Link></div>
         </div>;
     } else {
       displayListNav = 'none';
       barContents = '';
     }
     let optionsList = [];
+    let hambHeight;
+    if (this.props.menuSymbol === 'menu') {
+      hambHeight = '14vmin';
+    } else {
+      hambHeight = '20vmin';
+    }
 
     if (this.state.searchList === 'parents') {
       let parentList = Object.entries(this.props.lists[this.state.searchList]);
@@ -87,13 +121,19 @@ class Header extends React.Component {
             color: var(--mainBg);
           }
           #hamburger {
+            position: absolute;
+            right: 2%;
+            top: 0;
             text-align: center;
             width: 16vmin;
-            height: 16vmin;
+            height: ${hambHeight};
             background-color: #191919;
             display: inline-flex;
-            align-items: center;
+            align-items: flex-start;
             justify-content: center;
+            margin-top: 2%;
+            margin-right: 2%;
+            z-index: 0;
           }
           #header {
             background-color: var(--darkest);
@@ -109,17 +149,22 @@ class Header extends React.Component {
             font-family: Playfair Display; serif;
             width: 100%;
             color: var(--darkAccent);
-            display: inline-flex;
+            display: flex;
             justify-content: space-around;
             align-items: center;
             padding: 2%;
             flex-wrap: wrap;
           }
           #admin-section-nav {
+            font-size: 2rem;
+            font-family: Tangerine; cursive;
+            text-align: right;
+            width: 100%;
             display: ${displayListNav};
-            background-color: #222;
-            margin-top: 1rem;
+            background-color: #191919;
+            margin-top: 0.5rem;
             padding: 1rem;
+            z-index: 1;
           }
           .tiny {
             text-align: right;
@@ -137,6 +182,7 @@ class Header extends React.Component {
             margin-bottom: 0.25rem;
           }
           #search-area {
+            margin-top: 1rem;
             display: inline-flex;
             align-items: center;
             justify-content: space-around;
@@ -165,6 +211,9 @@ class Header extends React.Component {
           </div>
         </div>
         <div id="admin-nav-bar">
+          <div id="admin-section-nav">
+            {barContents}
+          </div>
           <form onSubmit={() => this.props.onSubmitSearch(event, this.state.searchTerm.value, this.state.searchList)}>
             <div id="search-area">
               <input
@@ -185,152 +234,11 @@ class Header extends React.Component {
               <div><input onClick={() => this.changeSearchList('appointments')} type='radio' name='search-type' value='appointments'></input>Appointments</div>
             </div>
           </form>
-          <div id="admin-section-nav">
-            {barContents}
-          </div>
         </div>
       </div>
     );
   }
 }
-
-// function Header(props) {
-
-// let barContents;
-// if (props.menuSymbol === 'close') {
-//   barContents = <div id="admin-section-nav">
-//   <Link to="/employees">Employees</Link> | <Link to="/parents">Parents</Link> | <Link to="/pets">Pets</Link> | <Link to="/appointments">Appointments</Link>
-// </div>
-// } else {
-//   barContents = '';
-// }
-// let optionsList;
-// if (this.state.searchList === 'pets') {
-//   optionsList = [
-//     'Megatron',
-//     'Cheddar Cheese'
-//   ];
-// } else if (this.state.searchList === 'parents') {
-//   optionsList = [
-//     'Reeves',
-//     'Schwarzenegger'
-//   ];
-// }
-
-// let suggestOptions = 
-// optionsList.map((optionString, i) => 
-//   <option key={i} value={optionString} />
-// )
-
-// return (
-//   <div id="header">
-//     <style jsx>{`
-//       .material-icons {
-//         font-size:2.5rem;
-//       }
-//       #top-row {
-//         width:100%;
-//         display: inline-flex;
-//         align-items: center;
-//         justify-content: space-between;
-//       }
-//       #logo, .tiny {
-//         color: var(--mainBg);
-//       }
-//       #hamburger {
-//         text-align: center;
-//         width: 16vmin;
-//         height: 16vmin;
-//         background-color: #191919;
-//         display: inline-flex;
-//         align-items: center;
-//         justify-content: center;
-//       }
-//       #header {
-//         background-color: var(--darkest);
-//         color: var(--mainBg);
-//         box-sizing: border-box;
-//         font-family: Tangerine; cursive;
-//         font-size: 2.5rem;
-//         width: 100%;
-//         padding: 2%;
-//       }
-//       #admin-nav-bar {
-//         font-size: 1rem;
-//         font-family: Playfair Display; serif;
-//         width: 100%;
-//         color: var(--darkAccent);
-//         display: inline-flex;
-//         justify-content: space-between;
-//         align-items: center;
-//         background-color: #222;
-//         padding: 2%;
-//         flex-wrap: wrap;
-//       }
-//       .tiny {
-//         text-align: right;
-//         font-size: 0.6rem;
-//         font-family: sans-serif;
-//         margin-top: 0.25rem;
-//       }
-//       #search {
-//         height: 2rem;
-//         font-size: 1.5rem;
-//         padding-left: 0.5rem;
-//         margin-right: 0.25rem;
-//       }
-//       #search-area {
-//         display: inline-flex;
-//         align-items: center;
-//         justify-content: space-between;
-//       }
-//       #search-options {
-//         width:85%;
-//         align-items: center;
-//         display: inline-flex;
-//         justify-content: space-between;
-//       }
-//       button {
-//         padding: 0;
-//       }
-//     `}</style>
-//     <div id='top-row'>
-//       <Link to="/">
-//         <div id="logo">
-//           {props.displayTitle}
-//         </div>
-//         <div className='tiny'>Administrative Portal v0.1</div>
-//       </Link>
-//       <div onClick={props.onClickHamburger} id='hamburger'>
-//         <i className="material-icons"><big>{props.menuSymbol}</big></i>
-//       </div>
-//     </div>
-//     <div id="admin-nav-bar">
-//       <form onSubmit={() => props.onSubmitSearch(event,this.state.searchTerm.value,this.state.searchList)}>
-//         <div id="search-area">
-//           <input
-//             list='found-entries'
-//             autoComplete='off'
-//             ref={(input) => { this.state.searchTerm = input; }}
-//             type='text'
-//             placeholder='Search...'
-//             id='search' />
-//           <datalist id='found-entries'>
-//             {suggestOptions}
-//           </datalist>
-//           <button type="submit"><i className="material-icons">search</i></button>
-//         </div>
-//         <div id='search-options'>
-//           <div><input defaultChecked onClick={() => this.changeSearchList('parents')} type='radio' name='search-type' value='parents'></input>Parents</div>
-//           <div><input onClick={() => this.changeSearchList('pets')} type='radio' name='search-type' value='pets'></input>Pets</div>
-//           <div><input onClick={() => this.changeSearchList('appointments')} type='radio' name='search-type' value='appointments'></input>Appointments</div>
-//         </div>
-//       </form>
-//       {barContents}
-//     </div>
-//   </div>
-// );
-// }
 
 Header.propTypes = {
   menuSymbol: PropTypes.string,
