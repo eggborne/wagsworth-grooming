9c6537b45fb3b5d663dde1e92551c3f52b3dba53
Michael Donovan <mike@eggborne.com>
2018-11-15 20:22:35 -0800

    Add 'nothing found' message, adjust style

--- src/components/cards/PetCard.jsx
+++ src/components/cards/PetCard.jsx
@@ -3,48 +3,54 @@ import PropTypes from 'prop-types';
 var moment = require('moment');
 
 function PetCard(props) {
-  let obj = props.entryObject;
-  let timeInfo = displayTimeCreatedInfo(obj.dateCreated);
-  return (
-    <div style={props.style.div}>
-      <style jsx>{`
-        div:nth-child(2n) {
-          background-color: var(--darkBg);
-        }
-        a {
-          color: blue;
-        }
-      `}</style>
-      <h2>{obj.name} <a href='./#/parents'>{props.printAssociatedEntryLink('parents',obj.parent)}</a></h2>
-      <h3>Breed:</h3>
-      <ul>
-        {obj.breed.map((breed, i) => 
-          <li key={i}>{breed}</li>
-        )}
-      </ul>
-      <h3>Sex: {obj.sex}</h3>
-      <h3>Weight: {obj.weight} | Color: {obj.color}</h3>
-      <h3>DOB: {obj.dob}</h3>
-      <h3>Vax date: {obj.vaccinationDate}</h3>
-      <h3>Vax clinic: {obj.vaccinationClinic}</h3>
-      <h3>Vet clinic: {obj.veterinarian}</h3>
-      <h3>Appointments: </h3>
-      <ul>
-        {obj.appointments.map((appt, index) =>
-          <li key={index}><big><a href={'./#/appointments'}>{props.printAssociatedEntryLink('appointments',obj.appointments[index])}</a></big></li>
-        )}
-      </ul>
-      <h3>Notes:</h3>
-      <ul>
-        {obj.notes.map((note, index) =>
-          <li key={index}>{note}</li>
-        )}
-      </ul>
-      <small style={props.style.small}>Created {timeInfo.dateCreated}</small><br />
-      <small style={props.style.small}>({timeInfo.timeSinceCreated})</small><br />
-      <small style={props.style.small}>Unique ID: {obj.id}</small>
-    </div>
-  );
+  if (!props.entryObject.id) {
+    return (
+      <div id='no-result-message'>NOTHING FOUND :(</div>
+    );
+  } else {
+    let obj = props.entryObject;
+    let timeInfo = displayTimeCreatedInfo(obj.dateCreated);
+    return (
+      <div style={props.style.div}>
+        <style jsx>{`
+          div:nth-child(2n) {
+            background-color: var(--darkBg);
+          }
+          a {
+            color: blue;
+          }
+        `}</style>
+        <h2>{obj.name} <a href='./#/parents'>{props.printAssociatedEntryLink('parents', obj.parent)}</a></h2>
+        <h3>Breed:</h3>
+        <ul>
+          {obj.breed.map((breed, i) =>
+            <li key={i}>{breed}</li>
+          )}
+        </ul>
+        <h3>Sex: {obj.sex}</h3>
+        <h3>Weight: {obj.weight} | Color: {obj.color}</h3>
+        <h3>DOB: {obj.dob}</h3>
+        <h3>Vax date: {obj.vaccinationDate}</h3>
+        <h3>Vax clinic: {obj.vaccinationClinic}</h3>
+        <h3>Vet clinic: {obj.veterinarian}</h3>
+        <h3>Appointments: </h3>
+        <ul>
+          {obj.appointments.map((appt, index) =>
+            <li key={index}><big><a href={'./#/appointments'}>{props.printAssociatedEntryLink('appointments', appt)}</a></big></li>
+          )}
+        </ul>
+        <h3>Notes:</h3>
+        <ul>
+          {obj.notes.map((note, index) =>
+            <li key={index}>{note}</li>
+          )}
+        </ul>
+        <small style={props.style.small}>Created {timeInfo.dateCreated}</small><br />
+        <small style={props.style.small}>({timeInfo.timeSinceCreated})</small><br />
+        <small style={props.style.small}>Unique ID: {obj.id}</small>
+      </div>
+    );
+  }
 }
 
 function displayTimeCreatedInfo(dateCreated) {
