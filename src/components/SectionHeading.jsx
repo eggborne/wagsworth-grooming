305d431f73af84762417a045156a63612b7e0d90
Michael Donovan <mike@eggborne.com>
2018-11-16 00:58:21 -0800

    Change onClickToUpdateList to pass event

--- src/components/SectionHeading.jsx
+++ src/components/SectionHeading.jsx
@@ -111,7 +111,7 @@ function SectionHeading(props) {
           </div>
           <div id='btns'>
             <button onClick={() => props.onRequestNewEntryForm(event, props.type)}><i className='material-icons'>{addIcon}</i></button>
-            {/* <button onClick={() => props.onClickToUpdateList(props.type)}><i className='material-icons'>refresh</i></button> */}
+            <button onClick={() => props.onClickToUpdateList(event, props.type)}><i className='material-icons'>refresh</i></button>
           </div>
         </div>
         <div id='sort-style-radios'>
