10088eb31adac3cd795bf285bc96362066a485f5
Michael Donovan <mike@eggborne.com>
2018-11-17 06:46:18 -0800

    Get sortOptions from props.entryAttributes, move styles to global

--- src/components/SectionHeading.jsx
+++ src/components/SectionHeading.jsx
@@ -6,31 +6,7 @@ function SectionHeading(props) {
   let sortOptions;
   let addIcon = 'person_add';
   let radius;
-  if (props.type === 'parents') {
-    sortOptions = [
-      'Name',
-      'Last Appt',
-      'Created'
-    ];
-  } else if (props.type === 'pets') {
-    sortOptions = [
-      'Name',
-      'Parent',
-      'Created'
-    ];
-  } else if (props.type === 'appointments') {
-    sortOptions = [
-      'Date',
-      'Parent',
-      'Pet'
-    ];
-  } else {
-    sortOptions = [
-      'Date',
-      'Parent',
-      'Pet'
-    ];
-  }
+  sortOptions = props.entryAttributes[props.type].sortOptions;
 
   if (props.type === 'employees') {
     radius = '0.5rem';
@@ -38,62 +14,10 @@ function SectionHeading(props) {
     radius = '0rem 0.5rem 0.5rem 0.5rem';
   }
   return (
-    <div>
+    <div id='section-heading'>
       <style jsx>{`
         div {
-          box-sizing:border-box;
-          border-radius: 0.5rem;
-          background-color: var(--dark);
-          color: var(--mainBg);
-          width: 100%;
-          padding: 2%;
           border-radius: ${radius};
-          font-family: Helvetica;
-          font-size: 1rem;
-        }
-        #lbl {
-          width: 35%;
-        }
-        #btns {
-          padding: 0;
-          display: inline-flex;
-          justify-content: flex-end;
-        }
-        button {
-          border-radius: 0.4rem;
-          margin: 0.25rem;
-        }
-        #top-buttons {
-          padding: 0;
-          display: inline-flex;
-          align-items: center;
-          justify-content: space-between;
-        }
-        #sort-label {
-          display: inline-flex;
-          align-items: center;
-        }
-        #sort-select {
-          border-radius: 0.4rem;
-          padding: 0.5rem;
-          justify-self: flex-start;
-          margin-left: 0.25rem;
-        }
-        #sort-style-radios {
-          display: inline-flex;
-          border-radius: 0;
-          padding: 0.25rem;
-        }
-        #sort-style-radios > div {
-          width: 6rem;
-          border-radius: 0;
-          padding: 0;
-          display: inline-flex;
-          align-items: center;
-          font-size: 0.8rem;
-        }
-        input:nth-child(2n) {
-          margin-left: 1rem;
         }
       `}</style>
       <form>
@@ -111,12 +35,12 @@ function SectionHeading(props) {
           </div>
           <div id='btns'>
             <button onClick={() => props.onRequestNewEntryForm(event, props.type)}><i className='material-icons'>{addIcon}</i></button>
-            <button onClick={() => props.onClickToUpdateList(event, props.type)}><i className='material-icons'>refresh</i></button>
+            <button onClick={() => props.onClickToUpdateList(event, props.type, 'dateCreated')}>All <i className='material-icons'>refresh</i></button>
           </div>
         </div>
         <div id='sort-style-radios'>
-          <div onClick={() => props.onChangeSortStyle(event, '')}><input defaultChecked type='radio' name='sort-order' value=''></input>Ascending</div>
-          <div onClick={() => props.onChangeSortStyle(event, 'desc')}><input type='radio' name='sort-order' value='desc'></input>Descending</div>
+          <div onClick={() => props.onChangeSortStyle(event, '')}><input defaultChecked className='visible' type='radio' name='sort-order' value=''></input>Ascending</div>
+          <div onClick={() => props.onChangeSortStyle(event, 'desc')}><input className='visible' type='radio' name='sort-order' value='desc'></input>Descending</div>
         </div>
       </form>
     </div>
@@ -125,6 +49,7 @@ function SectionHeading(props) {
 
 SectionHeading.propTypes = {
   type: PropTypes.string,
+  entryAttributes: PropTypes.object,
   onRequestNewEntryForm: PropTypes.func,
   onClickToUpdateList: PropTypes.func,
   onChangeSortBy: PropTypes.func,
