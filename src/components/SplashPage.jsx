3a868d3ae3a6339f417668b95cd4ac2bd98c1060
Michael Donovan <mike@eggborne.com>
2018-11-22 19:43:42 -0800

    Move CSS to global sheet

--- src/components/SplashPage.jsx
+++ src/components/SplashPage.jsx
@@ -14,30 +14,7 @@ function SplashPage() {
   }, 10);
   
   return (
-    <div>
-      <style jsx>{`
-        div {
-          padding: 2%;
-          margin:1.5%;
-          text-align: center;
-          border-radius: 0.5rem;
-          background-color: var(--mainBg);
-        }
-        img {
-          width: 60vmin;
-          border: 1px solid #333;
-          border-radius: 4%;
-          margin: 10%;
-          margin-top: 5%;
-          opacity: 0;
-          transform: scale(0);
-          transition: all 600ms ease;
-        }
-        #logo-msg {
-          transform: scale(0);
-          transition: all 600ms ease;
-        }
-      `}</style>
+    <div id='splash-page'>
       <div id='logo-msg'><h1>Look at this dog</h1></div>
       <img id='logo-img' src={headerImage} />
     </div>
